6dfe6956e895b4142751a11b492fd703af2ff083
imrochamatheus <im.rochamatheus@gmail.com>
2022-06-25 00:44:32 -0300

    style: Estilizada página home

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,41 +10,56 @@ import {
 import { Link } from "react-router-dom";
 import { sections } from "./sections";
 import { Icon } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { linkMotion, iconMotion } from "./animationsConfig";
 
 const Nav = () => {
   return (
-    <Flex
-      py={4}
-      justify="center"
-      position="absolute"
-      bottom={0}
-      w="100%"
-      color="whiteAlpha.700"
-      borderTop="1px solid"
-      borderColor="whiteAlpha.400"
-    >
-      <Breadcrumb fontWeight="medium" fontSize="sm" spacing={8} separator="">
-        {sections.map(({ label, url, icon }, i) => (
-          <BreadcrumbItem key={i}>
-            <BreadcrumbLink as={Link} to={url} _hover={{ color: "purple.400" }}>
-              <Stack align="center" direction={{ base: "column", md: "row" }}>
-                <StackItem>
-                  <Icon as={icon} fontSize={24} />
-                </StackItem>
-                <StackItem>
-                  <Text
-                    fontFamily={"heading"}
-                    fontSize={{ base: "sm", md: "md" }}
-                  >
-                    {label}{" "}
-                  </Text>
-                </StackItem>
-              </Stack>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        ))}
-      </Breadcrumb>
-    </Flex>
+    <AnimatePresence>
+      <Flex
+        py={4}
+        justify="center"
+        position="absolute"
+        bottom={0}
+        w="100%"
+        color="whiteAlpha.700"
+        borderTop="1px solid"
+        borderColor="whiteAlpha.400"
+      >
+        <Breadcrumb fontWeight="medium" fontSize="sm" spacing={8} separator="">
+          {sections.map(({ label, url, icon }, i) => (
+            <BreadcrumbItem
+              key={i}
+              as={motion.div}
+              initial="rest"
+              whileHover="hover"
+              animate="rest"
+              variants={linkMotion}
+            >
+              <BreadcrumbLink
+                as={Link}
+                to={url}
+                _hover={{ color: "purple.400" }}
+              >
+                <Stack align="center" direction={{ base: "column", md: "row" }}>
+                  <StackItem as={motion.div} variants={iconMotion}>
+                    <Icon as={icon} fontSize={24} color="white.500" />
+                  </StackItem>
+                  <StackItem>
+                    <Text
+                      fontFamily={"heading"}
+                      fontSize={{ base: "sm", md: "md" }}
+                    >
+                      {label}{" "}
+                    </Text>
+                  </StackItem>
+                </Stack>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          ))}
+        </Breadcrumb>
+      </Flex>
+    </AnimatePresence>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import { Flex, Heading, Stack, StackItem } from "@chakra-ui/react";
-import Nav from "../../components/Nav";
-import Typewriter from "typewriter-effect";
 import { Image } from "@chakra-ui/react";
 
+import Nav from "../../components/Nav";
+import Typewriter from "typewriter-effect";
+
 import profile from "../../assets/img/home-animation.gif";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
@@ -22,29 +24,57 @@ const Home = () => {
         align="center"
       >
         <StackItem>
-          <Image src={profile} width={300} />
+          <Image
+            src={profile}
+            w={{ base: 250, md: 300 }}
+            as={motion.img}
+            initial={{ marginLeft: "-500px" }}
+            animate={{ marginLeft: 0 }}
+            exit={{ marginLeft: "-500px" }}
+            transition="all .2s"
+          />
         </StackItem>
+
         <StackItem>
-          <Stack spacing={0}>
-            <StackItem>
-              <Heading fontSize={{ base: "md", md: "xl" }}>
-                Olá, eu sou o
+          <Stack spacing={1}>
+            <StackItem display="flex" alignItems="flex-end">
+              <Heading
+                fontSize={{ base: "xl", md: "5xl" }}
+                fontWeight={300}
+                mb={{ md: -1, lg: -2 }}
+              >
+                Olá,&nbsp;
+              </Heading>
+              <Heading
+                fontSize={{ base: "md", md: "xl" }}
+                fontWeight={300}
+                variant="span"
+              >
+                eu sou o
               </Heading>
             </StackItem>
+
             <StackItem>
               <Heading
                 textShadow="4px 4px #ff0000, -4px -4px #9F7AEA"
-                fontSize={{ base: "3xl", md: "6xl" }}
+                fontSize={{ base: "3xl", md: "4xl", lg: "6xl" }}
               >
                 Matheus Rocha!
               </Heading>
             </StackItem>
+
             <StackItem>
-              <Heading fontSize={{ base: "md", md: "xl" }}>{"<dev/>"}</Heading>
+              <Heading
+                fontSize={{ base: "md", md: "3xl" }}
+                fontWeight={300}
+                color="whiteAlpha.600"
+              >
+                {"<dev/>"}
+              </Heading>
             </StackItem>
 
             <StackItem>
-              <Heading fontSize={{ base: "md", md: "xl" }}>
+              <Heading fontSize={{ base: "sm", md: "xl" }} fontWeight={300}>
                 <Typewriter
                   options={{
                     strings: [
